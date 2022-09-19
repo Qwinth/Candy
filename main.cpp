14bdeb529eeba4c43d97a03eda4cb06dd12d256c
Qwinth <76917271+Qwinth@users.noreply.github.com>
2022-09-19 12:22:15 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
 #define _NO_DEBUG_HEAP
-#include <iostream>
-#include <vector>
-#include <map>
 #ifdef _WIN32
 #include <Windows.h>
 #endif
+#include <iostream>
+#include <vector>
+#include <map>
 #include <fstream>
 #include <sstream>
 #include <vector>
 #include <map>
 #include <algorithm>
+#include <cmath>
+#include "libs/strlib.hpp"
 #include "classes/token.hpp"
 #include "classes/lexer.hpp"
 #include "classes/parser.hpp"
@@ -21,7 +23,8 @@ int main(int argc, char * argv[]){
     SetConsoleOutputCP(CP_UTF8);
 #endif
     if (argc > 1) {
-        ifstream file(argv[1]);
+        string target = argv[1];
+        ifstream file(target);
         stringstream code;
         code << file.rdbuf();
         file.close();
@@ -33,7 +36,10 @@ int main(int argc, char * argv[]){
             tokenlist[t] = l.tokenize(l.disassemble(codelines[t]));
         }
         code.str(std::string());
-        Parser p(tokenlist, argv[1]);
+        // for (auto i : tokenlist[21]) {
+        //     cout << i.type << " " << i.value << endl;
+        // }
+        Parser p(tokenlist, target);
         p.runCode();
     }
 
